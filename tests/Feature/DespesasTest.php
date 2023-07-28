67f89e1769eb251a6866aeaf690a3aadea0312e4
Joao Vitor <joaovitoralvares10@gmail.com>
2023-07-27 21:57:22 -0300

    test: certifica que usuario autenticado pode criar despesa

---
+++ tests/Feature/DespesasTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Despesa;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DespesasTest extends TestCase
+{
+    use RefreshDatabase, DatabaseMigrations;
+
+    public function test_usuario_autenticado_pode_criar_despesa()
+    {
+        $user = User::factory()->create();
+
+        $despesa = [
+            'descricao' => 'Gastos com Uber',
+            'data' => '2023-04-25',
+            'valor' => 200.58
+        ];
+
+        $response = $this->actingAs($user)->post('/api/despesas', $despesa);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseCount('despesas', 1);
+        $this->assertDatabaseHas('despesas', $despesa);
+        $this->assertTrue($user->despesas->contains(Despesa::first()));
+    }
+}
